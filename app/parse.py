3c5ec843335c134f2190b2aa3e611f8dc19125f3
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-04-18 15:35:47 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,8 +1,11 @@
 import csv
+from dataclasses import dataclass, fields
+from typing import List
+
 import requests
 from bs4 import BeautifulSoup
-from dataclasses import dataclass
-from typing import List
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -12,34 +15,37 @@ class Quote:
     tags: List[str]
 
 
-def get_quotes(page_url: str) -> List[Quote]:
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    text = quote_soup.select_one(".text").get_text(strip=True)
+    author = quote_soup.select_one(".author").get_text(strip=True)
+    tag_elements = quote_soup.select(".tag")
+    tags = [tag_element.get_text(strip=True) for tag_element in tag_elements]
+    return Quote(text, author, tags)
+
+
+def get_quotes_from_page(page_url: str) -> List[Quote]:
     response = requests.get(page_url)
     soup = BeautifulSoup(response.content, "html.parser")
     quote_elements = soup.select(".quote")
-    quotes = []
-    for quote_element in quote_elements:
-        text = quote_element.select_one(".text").get_text(strip=True)
-        author = quote_element.select_one(".author").get_text(strip=True)
-        tag_elements = quote_element.select(".tag")
-        tags = [tag_element.get_text(strip=True)
-                for tag_element in tag_elements]
-        quotes.append(Quote(text, author, tags))
+    quotes = [parse_single_quote(quote_element)
+              for quote_element in quote_elements]
     next_page = soup.select_one(".next > a")
     if next_page is not None:
         quotes.extend(
-            get_quotes("https://quotes.toscrape.com" + next_page["href"])
+            get_quotes_from_page(
+                "https://quotes.toscrape.com" + next_page["href"]
+            )
         )
     return quotes
 
 
 def main(output_csv_path: str) -> None:
-    quotes = get_quotes("https://quotes.toscrape.com")
+    quotes = get_quotes_from_page(BASE_URL)
     with open(output_csv_path, "w", newline="", encoding="utf-8") as csvfile:
-        fieldnames = ["text", "author", "tags"]
+        fieldnames = [field.name for field in fields(Quote)]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for quote in quotes:
-            writer.writerow(quote.__dict__)
+        writer.writerows([quote.__dict__ for quote in quotes])
 
 
 if __name__ == "__main__":
